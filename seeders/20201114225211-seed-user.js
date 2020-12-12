63e6727df776c015e44897c4a4942f5655e259c2
OBliteron-code <sssalazar90@gmail.com>
2020-12-12 17:23:05 -0500

    Archivos del backend

---
+++ seeders/20201114225211-seed-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('users', [{
+            name: 'carlos',
+            email: 'ejemplo@gmail.com',
+            password: '$2y$08$yg3YDZw0pKvC8wb53ehfGeXwoxIF6Lb7MtF3Qex.9OM6ic/cooyp2', //micontraseña
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]);
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('users', null, {});
+
+    }
+};
